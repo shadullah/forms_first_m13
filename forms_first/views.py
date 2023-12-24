dbf96880a1b83202a066d95cb7a7da8c78d0942c
hexaalif <shadullahsad9@gmail.com>
2023-12-24 12:55:30 +0600

    navbar set

--- forms_first/views.py
+++ forms_first/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, 'forms_first/home.html')
+
+def about(req):
+    return render(req, 'forms_first/about.html')
+
+def login(req):
+    return render(req, 'forms_first/login.html')
